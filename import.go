02810ef3a9f3d70e40ec46e9b59b2954bca2fd71
joechbed-OD <[200~233363293+joechbed-OD@users.noreply.github.com>
2025-10-06 23:13:19 +0000

    Hello to naming conventions

--- import.go
+++ import.go
@@ -1,19 +1,9 @@
-package main
+package basics
 
 import (
 	"fmt"
-	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello, Go standard library!")
-
-	resp, err := http.Get("https://www.google.com")
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	fmt.Println("HTTP Response Status:", resp.Status)
+	fmt.Println("Hello World")
 }
